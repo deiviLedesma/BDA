2f6c078e40995d3b77abdd30371b0d2da8769493
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2024-02-15 10:40:57 -0600

    Add files via upload

---
+++ Banco/BancoDominio/src/main/java/com/mycompany/bancodominio/Cliente.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bancodominio;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class Cliente {
+
+    private int clienteID;
+    private String nombre, apellidoP, apellidoM;
+    private String calle, numero, colonia;
+    private LocalDate fechaN;
+    private int edad;
+
+    public int getClienteID() {
+        return clienteID;
+    }
+
+    public void setClienteID(int clienteID) {
+        this.clienteID = clienteID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public LocalDate getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(LocalDate fechaN) {
+        this.fechaN = fechaN;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
