f940abd54c121cf66bd79eeb3a10c8e310d9ff15
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2024-02-20 05:22:46 -0700

    Add files via upload

---
+++ InterfazBanco/src/main/java/com/mycompany/interfazbanco/InterfazBanco.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfazbanco;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class InterfazBanco {
+
+    public static void main(String[] args) {
+        IniciarSesionFrame i = new IniciarSesionFrame();
+        i.show();
+    }
+}
